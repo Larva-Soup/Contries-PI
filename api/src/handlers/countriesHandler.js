e88bc18625c674000d41cda3ddcf473eb3e7840c
David <cdvdrosero@gmail.com>
2023-01-16 16:31:00 -0500

    trycatch added

--- api/src/handlers/countriesHandler.js
+++ api/src/handlers/countriesHandler.js
@@ -1,20 +1,32 @@
-const {getAllCountries, getCountryById, searchByName} = require('../controllers/countriesController')
+const {
+  getAllCountries,
+  getCountryById,
+  searchByName,
+} = require("../controllers/countriesController");
 
-const getCountriesHandler = async (req, res) =>{
-    const {name} = req.query;
+const getCountriesHandler = async (req, res) => {
+  try {
+    const { name } = req.query;
 
-    const results = name? await searchByName(name): await getAllCountries();
+    const results = name ? await searchByName(name) : await getAllCountries();
 
-    res.status(200).send(results)
+    res.status(200).send(results);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+};
+
+const getCountryHandler = async (req, res) => {
+try {
+    const { idPais } = req.params;
+
+  const results = await getCountryById(idPais.toUpperCase());
+
+  res.status(200).send(results);
+} catch (error) {
+    res.status(400).send({error: error.message})
 }
+  
+};
 
-const getCountryHandler = async (req, res) =>{
-    const{idPais} = req.params;
-
-    const results = await getCountryById(idPais.toUpperCase());
-
-    res.status(200).send(results)
-
-}
-
-module.exports = {getCountriesHandler, getCountryHandler}
+module.exports = { getCountriesHandler, getCountryHandler };
