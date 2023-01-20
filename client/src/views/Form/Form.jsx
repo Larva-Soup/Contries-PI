61b3c484bc890905433230e15e06ca22758d10d2
David <cdvdrosero@gmail.com>
2023-01-19 20:40:24 -0500

    validation progress

--- client/src/views/Form/Form.jsx
+++ client/src/views/Form/Form.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { getCountries } from "../../redux/actions";
 import { useSelector, useDispatch } from "react-redux";
+import { difficultyOptions, seasonOptions } from "./formHelpers";
 
 const Form = () => {
   const dispatch = useDispatch();
@@ -9,21 +10,22 @@ const Form = () => {
 
   const [form, setForm] = useState({
     name: "",
-    difficulty: "",
+    difficulty: "1",
     duration: "",
-    season: "",
+    season: "Primavera",
     countryArray: [],
   });
 
   const [time, setTime] = useState({
     hours: 0,
-    minutes: 0,
+    minutes: 1,
   });
 
   const [errors, setErrors] = useState({
     name: "",
     difficulty: "",
-    duration: "",
+    hours: "",
+    minutes: "",
     season: "",
     countryArray: "",
   });
@@ -45,32 +47,65 @@ const Form = () => {
   };
 
   const validateDifficulty = (form) => {
-    if(form.difficulty >=1 && form.difficulty <=5){
-      setErrors({...errors, difficulty: ""})
+    if (form.difficulty < 1 || form.difficulty > 5) {
+      setErrors({ ...errors, difficulty: "Please select a proper value" });
+      setForm({ ...form, difficulty: "que pasa" });
+      console.log(form);
     } else {
-      setErrors({...errors, difficulty: "Outside difficulty range"})
+      setErrors({ ...errors, difficulty: "" });
+    }
+  };
+
+  const validateHours = (time) => {
+    if (/^(2[0-3]|[01]?[0-9])$/gm.test(time.hours)) {
+      setErrors({...errors, hours:""});
+
+    } else {
+      setErrors({ ...errors, hours: "from 0 to 23 hours only" });
     }
   }
 
-  const validateDuration = (form) => {
+  const validateMinutes = (time) => {
+    // const regexcompleto = /^(2[0-3]|[01][0-9]):([0-5][0-9])$/gm
+    if(/^([0-5]?[0-9])$/gm.test(time.minutes)){
+      setErrors({...errors, minutes:""})
+    }else{
+      setErrors({...errors, minutes: "from 0 to 59 minutes only"})
+    }
+  };
 
+  const validateBoth = (time) => {
+    if(time.hours === "0" && time.minutes === "0"){
+      setErrors({...errors, minutes:"Please input a proper time"})
+      setTime({...time})
+    }/* if(time.hours > 0){
+      setErrors({...errors, minutes:""})
+    } */
   }
 
   const validateSeason = (form) => {
-    if(form.season === "Primavera" || form.season === "Verano" || form.season === "Otoño" || form.season === "Invierno"){
-      setErrors({...errors, season: ""})
+    if (
+      form.season === "Primavera" ||
+      form.season === "Verano" ||
+      form.season === "Otoño" ||
+      form.season === "Invierno"
+    ) {
+      setErrors({ ...errors, season: "" });
     } else {
-      setErrors({...errors, season: "Incorrect value"})
+      setErrors({ ...errors, season: "Incorrect value" });
     }
-  }
+  };
 
   const validateCountries = (form) => {
-    if(form.countryArray.length > 0){
-      setErrors({...errors, countryArray:""})
-    }else{
-      setErrors({...errors, countryArray:"Please select at least one country"})
+    if (form.countryArray.length > 0) {
+      setErrors({ ...errors, countryArray: "" });
+    } else {
+      setErrors({
+        ...errors,
+        countryArray: "Please select at least one country",
+      });
     }
-  }
+  };
 
   // const [countryName, setCountryName] = useState({id:"", name:""}) //pospuesto
 
@@ -81,19 +116,23 @@ const Form = () => {
   const changeTime = (e) => {
     const prop = e.target.name;
     const value = e.target.value;
-    let hours = 0;
-    let minutes = 0;
+    let hours;
+    let minutes;
 
     if (prop === "hours") {
       hours = e.target.value;
+      validateHours({ ...time, [prop]: value });
     }
     if (prop === "minutes") {
       minutes = e.target.value;
+      validateMinutes({ ...time, [prop]: value });
     }
 
-    setTime({ ...time, [prop]: value });
+    validateBoth({...time, [prop]: value})
 
-    return setForm({
+    setTime({...time, [prop]: value});
+
+    setForm({
       ...form,
       duration: `${hours || time.hours}:${minutes || time.minutes}`,
     });
@@ -103,21 +142,38 @@ const Form = () => {
     const prop = e.target.name;
     const value = e.target.value;
 
+    if (prop === "difficulty" && isNaN(value)) {
+      validateDifficulty({ ...form, [prop]: value });
+      return;
+    }
+
     prop === "name" && validateName({ ...form, [prop]: value });
-    prop === "difficulty" && validateDifficulty({...form, [prop]: value})
-    prop === "season" && validateSeason({...form, [prop]: value})
+    prop === "difficulty" && validateDifficulty({ ...form, [prop]: value });
+    prop === "season" && validateSeason({ ...form, [prop]: value });
 
     setForm({ ...form, [prop]: value });
   };
 
+  // const abortSubmit = () => {
+  //   if (!Object.values(errors).includes("")) {
+  //     console.log("no vas a ningún lado")
+  //     return;
+  //   }
+  // }
+
+  //se puede cambiar este
   const submitHandler = async (e) => {
     e.preventDefault();
+
     axios.post("http://localhost:3001/activities", form);
   };
 
   const handleCountrySelect = (e) => {
-    if(e.target.value === "") return;
-    validateCountries({...form, countryArray: [...form.countryArray, e.target.value] })
+    if (e.target.value === "") return;
+    validateCountries({
+      ...form,
+      countryArray: [...form.countryArray, e.target.value],
+    });
     setForm({ ...form, countryArray: [...form.countryArray, e.target.value] });
   };
 
@@ -125,32 +181,36 @@ const Form = () => {
     const arr = form.countryArray.filter(
       (country) => country !== e.target.value
     );
-    
-    validateCountries({...form, countryArray: arr })
+
+    validateCountries({ ...form, countryArray: arr });
     setForm({ ...form, countryArray: arr });
   };
-
-  // const filteredCountries = countries.filter((country)=>{
-  //   country.id ===
-  //  }
-  //   )
 
   return (
     <form onSubmit={submitHandler}>
       <div>
         <label>Activity Name: </label>
-        <input type="text" name="name" onChange={changeHandler} />
+        <input
+          type="text"
+          name="name"
+          onChange={changeHandler}
+          autoComplete="off"
+          value={form.name}
+        />
         {errors.name && <span>{errors.name}</span>}
       </div>
       <div>
         <label>Difficulty: </label>
-        <select name="difficulty" onChange={changeHandler}>
-          <option value="">-select difficulty-</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
+        <select
+          name="difficulty"
+          onChange={changeHandler}
+          value={form.difficulty}
+        >
+          {difficultyOptions.map((el) => (
+            <option value={el.value} key={el.label}>
+              {el.label}
+            </option>
+          ))}
         </select>
         {errors.difficulty && <span>{errors.difficulty}</span>}
       </div>
@@ -159,31 +219,31 @@ const Form = () => {
           <legend>Duration: </legend>
           <label>Hours: </label>
           <input
-            type="number"
-            min="0"
-            max="12"
+            type="text"
             name="hours"
             onChange={changeTime}
+            value={time.hours}
           />
+          {errors.hours && <span>{errors.hours}</span>}
           <label>Minutes:</label>
           <input
-            type="number"
-            min="0"
-            max="59"
+            type="text"
             name="minutes"
             onChange={changeTime}
+            value={time.minutes}
           />
+          {errors.minutes && <span>{errors.minutes}</span>}
         </fieldset>
       </div>
 
       <div>
         <label>Season: </label>
-        <select name="season" onChange={changeHandler}>
-          <option value="">-select season-</option>
-          <option value="Primavera">Spring</option>
-          <option value="Verano">Summer</option>
-          <option value="Otoño">Fall</option>
-          <option value="Invierno">Winter</option>
+        <select name="season" onChange={changeHandler} value={form.season}>
+          {seasonOptions.map((season) => (
+            <option value={season.value} key={season.label}>
+              {season.label}
+            </option>
+          ))}
         </select>
         {errors.season && <span>{errors.season}</span>}
       </div>
---
+++ client/src/views/Form/formHelpers.js
@@ -0,0 +1,51 @@
+const difficultyOptions = [
+  {
+    label: "-select difficulty-",
+    value: "",
+  },
+  {
+    label: "1",
+    value: "1",
+  },
+  {
+    label: "2",
+    value: "2",
+  },
+  {
+    label: "3",
+    value: "3",
+  },
+  {
+    label: "4",
+    value: "4",
+  },
+  {
+    label: "5",
+    value: "5",
+  },
+];
+
+const seasonOptions = [
+    {
+        label: "-select season-",
+        value: ""
+    },
+    {
+        label: "Spring",
+        value: "Primavera"
+    },
+    {
+        label: "Summer",
+        value: "Verano"
+    },
+    {
+        label: "Fall",
+        value: "Otoño"
+    },
+    {
+        label: "Winter",
+        value: "Invierno"
+    }
+]
+
+export { difficultyOptions, seasonOptions };
