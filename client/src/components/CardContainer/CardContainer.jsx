edd04b11b2a1bf8cb3ebc3a2066606371f49a0a0
David <cdvdrosero@gmail.com>
2023-01-24 11:17:51 -0500

    select tags added for filters

--- client/src/components/CardContainer/CardContainer.jsx
+++ client/src/components/CardContainer/CardContainer.jsx
@@ -2,12 +2,13 @@ import { useSelector } from "react-redux";
 import { useState } from "react";
 import Card from "../Card/Card";
 import Pagination from "../Pagination/Pagination";
+import {continents} from "./cardContainerHelper.js"
 
 const CardContainer = ({ loading }) => {
   const [currentPage, setCurretPage] = useState(1);
   const [postsPerPage] = useState(10);
 
-  const countries = useSelector((state) => state.countriesDefault);
+  const countries = useSelector((state) => state.countries);
 
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
@@ -17,6 +18,30 @@ const CardContainer = ({ loading }) => {
 
   return (
     <div>
+      <div>
+        <span>
+          <select>
+            {continents.map(continent => (
+              <option value= {continent.value} key={continent.value}>{continent.value}</option>
+            ))}
+          </select>
+        </span>
+        <span>
+          <select><option value="">--Select activity--</option>
+          <option value="clear">Everything</option></select>
+        </span>
+        <span>
+          <select>
+            <option value= "">--Order alphabetically--</option>
+            <option value="asc">Ascendent</option>
+          <option value="desc">Descendent</option></select>
+        </span>
+        <span>
+          <select><option value="">--Order by population--</option>
+          <option value="asc">Ascendent</option>
+          <option value="desc">Descendent</option></select>
+        </span>
+      </div>
       <>{loading && <h2>Loading...</h2>}</>
       {currentPosts.map((country) => {
         return (
---
+++ client/src/components/CardContainer/cardContainerHelper.js
@@ -0,0 +1,12 @@
+const continents = [
+    {value: "--Select continent--"},
+  { value: "North America" },
+  { value: "Africa" },
+  { value: "Asia" },
+  { value: "Europe" },
+  { value: "Oceania" },
+  { value: "South America" },
+  { value: "Antarctica" },
+];
+
+export {continents}
